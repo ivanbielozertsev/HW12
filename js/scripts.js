82d6f592091e22f968555b038e95064696dcda54
ivanbielozertsev <ivanbelozercev13@gmail.com>
2021-11-05 20:13:45 +0200

    Home Work 12

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+let value = prompt('Введите целое положительное число');
+
+function isPrime(num) {
+    for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+        return num > 1;
+    }
+}
+
+if (value === null || isNaN(value) || value.trim() === '' || value <= 0) {
+    alert('введенное значение не является целым положительным числом');
+} else if (!Number.isInteger(value)) {
+    alert('введенное значение является дробным числом');
+} else if (isPrime(value)) {
+    alert('простое число');
+} else {
+    alert('не простое число');
+}
